79205baa7bde99809e60be51990998ad8a5ccdee
Eric Edgar <rocketman110@gmail.com>
2013-07-25 10:50:19 -0500

    Update vmware_hgfs_fix.sh

--- vmware_hgfs_fix.sh
+++ vmware_hgfs_fix.sh
@@ -11,7 +11,7 @@ rm -rf vmhgfs-only
 tar xf vmhgfs.tar
 pushd vmhgfs-only/shared
 patch -p1 < $PATCH_DIR/vmware9.compat_mm.patch
-patch -p1 < $PATCH_DIR/vmware9.k3.9.vmhgfs.patch
+patch -p1 < $PATCH_DIR/vmware9.k3.9.patch
 popd
 tar cf vmhgfs.tar vmhgfs-only
 rm -rf vmhgfs-only
